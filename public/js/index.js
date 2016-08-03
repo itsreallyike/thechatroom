a7c687559248f4dd9ae0abcbfa689466cae28ca3
itsreallyike <IkeOrjiakorAir@Ikes-MacBook-Air.local>
2016-08-03 16:05:16 -0700

    Changes

--- public/js/index.js
+++ public/js/index.js
@@ -16,7 +16,7 @@ $("#login-btn").click(function() {
     var message1 = $('#message-box').val();
     var username = "'" + message1 + "'" + " logged in on" + " " + day + " at " + time
     socket.emit("login", username);
-    socket.emit("chat", username)
+    socket.emit("chat", message1)
     $('#messages').append($('<p>').text("you have logged in as '" + message1 + "' {" +time+ "}"));
     $('#message-box').val('');
     $('#message-box').attr("placeholder", "Dunjazz write a message here...").val('').focus().blur();
