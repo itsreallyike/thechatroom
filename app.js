4081748cd5e48f8b326e7653d7e804043504e5f9
itsreallyike <IkeOrjiakorAir@Ikes-MacBook-Air.local>
2016-08-03 15:29:15 -0700

    Change

--- app.js
+++ app.js
@@ -90,6 +90,7 @@ var user = [];
                 });
            };
         });
+            if(user.length - 1 > 1)
                 socket.broadcast.emit("chat", message);
     });
 });
--- public/js/index.js
+++ public/js/index.js
@@ -60,9 +60,6 @@ socket.on('chat', function (message) {
 
     if(users.length - 1 < 10) {
         $('#messages').prepend($('<p>').text(message))
-    } 
-    if(users.length - 1 === 10) {
-        return
     } else {
         $('#messages').append($('<p>').text(message));
     }
